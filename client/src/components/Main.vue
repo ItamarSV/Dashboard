6c7e8f3797238c73959177e2e8a07d85c3bb90f3
ItamarSvisa <itamarsvis@gmail.com>
2020-11-12 10:40:20 +0200

    BeforeUpdate

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -4,27 +4,28 @@
       <b-navbar toggleable="lg" type="dark" variant="info">
         <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-navbar-nav class=" mt-1" pills>
+            <b-nav-item class="mr-2" @click="Activenav('Home')" Active>Home</b-nav-item>
+            <b-nav-item class="mr-2" @click="Activenav('Data Analysis')">Data Analysis</b-nav-item>
+            <b-nav-item class="mr-2" @click="Activenav('Active')">Active</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
+            <!-- <b-nav-form>
               <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
               <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form>
+            </b-nav-form> -->
 
-            <b-nav-item-dropdown text="Lang" right>
+            <!-- <b-nav-item-dropdown text="Lang" right>
               <b-dropdown-item href="#">EN</b-dropdown-item>
               <b-dropdown-item href="#">ES</b-dropdown-item>
               <b-dropdown-item href="#">RU</b-dropdown-item>
               <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
+            </b-nav-item-dropdown> -->
 
             <b-nav-item-dropdown right>
               <!-- Using 'button-content' slot -->
@@ -38,32 +39,59 @@
         </b-collapse>
       </b-navbar>
     </div>
-    <button type="button" class="btn btn-primary">Main</button>
+    <button type="button" class="btn btn-primary">Main check</button>
+    <b-avatar icon="star-fill" size="4em"></b-avatar>
+    <b-avatar src="https://placekitten.com/300/300"></b-avatar>
+    <b-avatar src="dist/WebAvatar.jpg" size="6rem"></b-avatar>
+    <b-overlay :show="show" rounded="sm">
+      <itamartable v-if="activeNav==='Home'"></itamartable>
+    </b-overlay>
   </div>
 </template>
 
-
 <script>
+// import axios from 'axios';
+import Table from './Table.vue';
+
 export default {
-  data(){
-    return{
-      testdata:[],
+  data() {
+    return {
+      activeNav: 'Home',
+      show: true,
     };
   },
-  
-  methods:{
-    gettestdata(){
-      // const path = window.location.href.concat('/books');
-      const path = 'http://localhost:5000/main';
-      axios.get(path)
-        .then((res) => {
-          this.testdata = res.data.testdata;
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error);
-        });
+
+  components: {
+    itamartable: Table,
+  },
+
+  methods: {
+    Activenav(nav) {
+      this.activeNav = nav;
     },
+  },
+
+  created() {
   },
 };
 </script>
+<style scoped>
+.nav-pills .nav-link.active, .nav-pills .show>.nav-link {
+  background-color: transparent;
+  border-radius: 0rem;
+}
+.navbar-dark .navbar-nav .nav-link {
+    color: rgba(255, 255, 255);
+    border-bottom-style: solid;
+    border-color: transparent;
+}
+.navbar-dark .navbar-nav .nav-link:hover,
+.navbar-dark .navbar-nav .nav-link:focus,
+.navbar-dark .navbar-nav .nav-link:active{
+  color: rgba(255, 255, 255);
+  border-bottom-color: white !important;
+}
+a.nav-link {
+  color: white;
+}
+</style>
---
+++ client/src/components/Table.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <b-table hover :items="testdata"></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      testdata: [],
+    };
+  },
+
+  methods: {
+    gettestdata() {
+      // const path = window.location.href.concat('/books');
+      const path = 'http://localhost:5000/main';
+      axios.get(path)
+        .then((res) => {
+          this.testdata = res.data.testdata;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+
+  created() {
+    this.gettestdata();
+  },
+};
+</script>
